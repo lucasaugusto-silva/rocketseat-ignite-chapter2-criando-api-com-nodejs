9bb719fb923b23329c732de45c9b02e15ad66792
Lucas Silva <58057135+lucasaugusto-silva@users.noreply.github.com>
2022-05-24 17:01:20 -0300

    add rota get e post

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.json({ message: "Hello World" })
+})
+
+app.post('/courses', (req, res) => {
+    const { name } = req.body
+    return res.json({ name })
+})
+
+app.listen(3333, () => {
+    console.log('Server is running')
+})
+
