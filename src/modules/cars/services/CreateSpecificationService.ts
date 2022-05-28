fd8c12562c2f4d10ce9be7c1ecee5300fca31c2c
Lucas Silva <58057135+lucasaugusto-silva@users.noreply.github.com>
2022-05-28 13:52:10 -0300

    Adicionando repositorio de specification

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,17 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository"
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute() {
-        console.log('Todo')
+    constructor(private specificationsRepository: ISpecificationRepository) { }
+    execute({ name, description }: IRequest): void {
+        this.specificationsRepository.create({
+            name,
+            description
+        })
     }
 }
 
