a2143d9c2e910b3d21dbe3c59d2ee31dc9d706be
Lucas Silva~ <lucas.augusto7969@gmail.com>
2022-06-03 08:13:42 -0300

    Criando controller e useCase para o upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
+
 import multer from "multer";
 
 const categoriesRoutes = Router();
@@ -18,9 +20,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.send();
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
