e012b054ae336fa2253edb46409af33d2f26eb60
Lucas Silva <58057135+lucasaugusto-silva@users.noreply.github.com>
2022-06-29 13:45:21 -0300

    Corrigindo o erro network_mode is incompatible with port_bindings

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,7 +12,6 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (request, response) => {
-    console.log("Reload Funcionando")
     return createCategoryController.handle(request, response);
 });
 
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3333, () => {
-    console.log('Server is running');
+    console.log('Server is running na porta 3333');
 });
 
