1b43af12523e8a77961b5900277114798b5ab452
Lucas Silva~ <lucas.augusto7969@gmail.com>
2022-07-01 10:33:12 -0300

    Criando repositorio de usuario

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,12 @@
 import { container } from "tsyringe";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -13,4 +16,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );
